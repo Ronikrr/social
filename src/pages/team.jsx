139dd140d54546963fe0e90e66c4aac597fc88ee
Ronik <102733678+Ronikrr@users.noreply.github.com>
2024-11-28 16:59:59 +0530

    all

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Ourteam from '../components/ourteam/ourteam'
+import People from '../components/ourteam/people'
 const Team = () => {
     return (
         <div className='py-[122px] data_cla' >
             <Ourteam />
+            <People />
         </div>
     )
 }
