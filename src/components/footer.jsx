0d7722a3f20b3809e98cf75f06feb6c4d473fd85
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-02-26 16:54:19 +0530

    all

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaPinterestP } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
