22586356abdac2af9fd3896210648ac57fe81e66
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-02-27 14:35:12 +0530

    all

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Logo from '../assets/logo/logo.png'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link, useLocation } from 'react-router-dom';
 const Header = () => {
     const [isopenmenu, setisopenmenu] = useState(false);
@@ -23,6 +23,9 @@ const Header = () => {
     const hantogglemenu = () => {
         setisopenmenu(!isopenmenu);
     }
+    const handleToggleMenu = () => {
+        setisopenmenu(false)
+    }
 
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
@@ -44,7 +47,7 @@ const Header = () => {
     return (
         <div className="instrument">
             <div
-                className={`fixed top-0 hidden lg:flex left-0 w-full h-auto duration-1000  z-[99]  xl:py-[10px] px-[3vw] transition-all  ${scrolled ? 'bg-[#fff] py-[0px] shadow-md top-0 h-[80px] items-center  transition-all duration-1000 ' : 'bg-transparent  transition-all  duration-1000    '}`}
+                className={`fixed top-0 hidden lg:flex left-0 w-full h-auto  z-[99]  xl:py-[10px] px-[3vw]  ${scrolled ? 'bg-[#fff] py-[0px] shadow-md top-0 h-[80px] items-center ' : 'bg-transparent'}`}
             >
                 <div className="w-full">
                     <div className="relative flex items-center justify-between z-[5]">
@@ -58,8 +61,8 @@ const Header = () => {
                                     return (
                                         <li className="relative" key={id}>
                                             <Link className={`cursor-pointer 4xl:text-[20px]  xl:text-[17px] capitalize duration-1000 my-[5px] lg:mx-[5px] lg:p-[10px] xl:mx-[15px] font-medium xl:p-[15px] ${isActive
-                                                    ? 'text-[#ef4523] border-b-[2px] border-[#ef4523]'
-                                                    : 'hover:text-[#ef4523] hover:border-b-[2px] hover:border-[#ef4523]'
+                                                ? 'text-[#ef4523] border-b-[2px] border-[#ef4523]'
+                                                : 'hover:text-[#ef4523] hover:border-b-[2px] hover:border-[#ef4523]'
                                                 }`}
                                                 onClick={scrollToTop}
                                                 to={link.path}>
@@ -81,7 +84,13 @@ const Header = () => {
                                 <img src={Logo} className='h-[30px]' alt="" />
                             </a>
                             <div className="menu">
-                                <FaBars onClick={hantogglemenu} />
+                                {isopenmenu ? (
+                                    <FaTimes onClick={handleToggleMenu} />
+                                ) : (
+                                        <FaBars onClick={hantogglemenu} />
+                                )}
+
+
                             </div>
                         </div>
                         {isopenmenu && (
