318bc4b87ea396d0d28ded611d902a2770919fc5
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-03-01 16:12:46 +0530

    all

--- src/components/contactfrom.jsx
+++ src/components/contactfrom.jsx
@@ -107,17 +107,17 @@ const ContactForm = () => {
             </div>
             <div className="mt-10 w-full h-[400px] lg:h-[500px]">
 
-            <iframe
-                title="WebSolex Infotech Location"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3844.841972345225!2d72.87119960000001!3d21.234213999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04f1bdb2a9af7%3A0xf2671d4491f5d464!2sWebSolex%20Infotech!5e1!3m2!1sen!2sin!4v1740566154024!5m2!1sen!2sin"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
+                <iframe
+                    title="WebSolex Infotech Location"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3844.841972345225!2d72.87119960000001!3d21.234213999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04f1bdb2a9af7%3A0xf2671d4491f5d464!2sWebSolex%20Infotech!5e1!3m2!1sen!2sin!4v1740566154024!5m2!1sen!2sin"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
                     allowFullScreen
                     className="filter brightness-70 contrast-100 "
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>  
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
             </div>
         </div>
     );
