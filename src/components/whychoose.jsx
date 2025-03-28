5809b2ea455fc733d6e41f13c132e3993dbcdf05
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-03-28 16:06:45 +0530

    first commit

--- src/components/whychoose.jsx
+++ src/components/whychoose.jsx
@@ -136,7 +136,7 @@ const Whychoose = () => {
                 className={`absolute w-full h-full lg:h-[100vh] 2xl:h-[175vh] -z-10 transition-opacity duration-500 ${isLoaded ? "opacity-100" : "opacity-0"
                     }`}
                 style={{
-                    backgroundImage: `url(${isLoaded ? box : "data:image/svg+xml;base64,..."})`, // Placeholder
+                    backgroundImage: `url(${isLoaded ? box : "https://www.t3bucket.com/f/0-boxes(1).png"})`, // Placeholder
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                 }}
